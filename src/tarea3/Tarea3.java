4b8ec97e43d5bf6a4278d80b7019dc96d30a5a9f
Merari <Merari@DESKTOP-Q8BBL7C>
2017-08-07 15:40:17 -0600

    Tarea 3 (1.1)

---
+++ src/tarea3/Tarea3.java
@@ -0,0 +1,18 @@
+
+package tarea3;
+
+/**
+ *
+ * @author Merari
+ */
+public class Tarea3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    m_principal o = new m_principal();  
+    o.menuPrincipal();
+    }
+    
+}
---
+++ src/tarea3/m_principal.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Merari
+ */
+public class m_principal {
+    
+    int opcion;
+    Scanner lector;
+    String usuarios[];
+    boolean bandera;
+    
+    public m_principal(){
+        opcion = 0;
+        lector = new Scanner(System.in);  //se inicia aqui en el constructor porque es global
+        usuarios = new String[5];
+        bandera = false;
+        
+    }
+    
+    public void menuPrincipal(){
+        do{
+        System.out.println("Tarea 3");
+        System.out.println("201503565");
+        System.out.println("Seleccione una opcion: ");
+        System.out.println("1. Usuarios");
+        System.out.println("2. Palabras Palíndromas");
+        System.out.println("3. Salir");
+ 
+            opcion = lector.nextInt();
+            switch(opcion){
+                case 1:
+                    usuarios();
+                    break;
+                case 2:
+                    palabras_palindromas();
+                    break;
+                case 3:
+                    System.exit(0);
+            }        
+        }while(opcion != 3);
+    }
+
+    private void usuarios() {
+       do{
+           System.out.println("\n");
+           System.out.println("Menú de Usuarios");
+           System.out.println("1. Ingresar Usuarios");
+           System.out.println("2. Mostrar todos los Usuarios");
+           System.out.println("3. Mostrar un Usuario Personalizado");
+           System.out.println("4. Menú Principal");
+           System.out.println("5. Salir");
+           
+           opcion = lector.nextInt();
+           switch(opcion){
+               case 1:
+                   creaUsuario();
+                   break;
+               case 2:
+                   muestraUsuarios();
+                   break;
+               case 3:
+                   persUsuario();
+                   break;
+               case 4:
+                   menuPrincipal();
+                   break;
+               case 5:
+                   System.exit(0);
+           }
+           
+       }while(opcion != 5);
+    }
+
+    private void palabras_palindromas() {
+        String palabra;
+        System.out.println("Ingrese una palabra: ");
+        palabra = this.lector.next();
+        if(palabra.equals(usuarios[0])){
+            
+        }
+    }
+
+    private void creaUsuario() {
+        int actual;
+        for(actual = 0; actual < 5; actual++ ){
+            System.out.println("Ingrese el nombre del usuario " + (actual+1) + ":");
+            usuarios[actual] = this.lector.next();
+        }
+        System.out.println("Usuarios creados exitosamente.");
+    }
+
+    private void muestraUsuarios() {
+        System.out.println("Los usuarios actuales son: ");
+        int actual;
+        for(actual = 0; actual < 5; actual++){
+            System.out.println("Usuario " + (actual+1) + " :");
+            System.out.println(usuarios[actual] + "\n");
+        }
+    }
+
+    private void persUsuario() {
+        
+    }
+    
+}
